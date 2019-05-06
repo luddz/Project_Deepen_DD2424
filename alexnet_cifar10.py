13bfae83d07ec61a5e296345700cbfd6ce64a224
rouwayd <rouwayd@hotmail.com>
2019-05-06 14:18:48 +0200

    Add load_model function

--- alexnet_cifar10.py
+++ alexnet_cifar10.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import keras
 from keras.datasets import cifar10
 from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 import os
